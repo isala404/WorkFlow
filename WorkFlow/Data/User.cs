743e8cb9e67f0ecf211bab1686244333187643d6
Isala Piyarisi <code@isala.me>
2021-11-28 16:16:26 +0530

    feat: created RUD UIs for User

--- WorkFlow/Data/User.cs
+++ WorkFlow/Data/User.cs
@@ -1,5 +1,13 @@
 namespace WorkFlow.Data
 {
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+    }
+
     public enum UserRole
     {
         USER,
