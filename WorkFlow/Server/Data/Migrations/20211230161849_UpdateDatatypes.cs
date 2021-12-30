1fe26c72431b66adc8961b9650cd2e6d1eaa3035
Isala Piyarisi <code@isala.me>
2021-12-30 21:49:24 +0530

    fix: Added Entity Framework based validation

---
+++ WorkFlow/Server/Data/Migrations/20211230161849_UpdateDatatypes.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkFlow.Server.Migrations
+{
+    public partial class UpdateDatatypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tickets",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Uri",
+                table: "Projects",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Projects",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Uri",
+                table: "Companies",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Companies",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tickets",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Uri",
+                table: "Projects",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Uri",
+                table: "Companies",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25,
+                oldNullable: true);
+        }
+    }
+}
--- WorkFlow/Shared/Entities/Ticket.cs
+++ WorkFlow/Shared/Entities/Ticket.cs
@@ -5,14 +5,32 @@ namespace WorkFlow.Shared.Entities
 {
     public class Ticket
     {
-        [Key] public Guid Id { get; set; }
-        [Required] public String Name { get; set; }
-        [Required] public String Description { get; set; }
-        [Required] public Priority Priority { get; set; }
-        [Required] public Status Status { get; set; } = Status.ToDo;
-        [Required] public User? Assignee { get; set; }
-        [Required] public DateTime DueDate { get; set; }
+        [Key]
+        public Guid Id { get; set; }
+        
+        [Required]
+        [StringLength(25, MinimumLength = 3, ErrorMessage="Name must be at least 3 and at max 25 characters long")]
+        public String Name { get; set; }
+        
+        [Required]
+        [MaxLength]
+        public String Description { get; set; }
+        
+        [Required]
+        public Priority Priority { get; set; }
+        
+        [Required]
+        public Status Status { get; set; } = Status.ToDo;
+        
+        [Required]
+        public User? Assignee { get; set; }
+        
+        [Required]
+        public DateTime DueDate { get; set; }
+        
         public TimeSpan EstimatedTime { get; set; }
-        [Required] public Project? Project { get; set; }
+        
+        [Required]
+        public Project? Project { get; set; }
     }
 }
